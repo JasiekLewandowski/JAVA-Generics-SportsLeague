2c64633369a56eefcb34a79a8413604554d55fb0
Jasiek <lewandowskifilm@gmail.com>
2020-08-11 12:13:41 +0100

    Added few corrections.

--- src/League.java
+++ src/League.java
@@ -3,22 +3,22 @@ import java.util.Collections;
 
 public class League<T extends Team>   {
     private String name;
+    private ArrayList<T> leagueBoard = new ArrayList<>();
 
     public League(String name) {
         this.name = name;
     }
-    ArrayList<T> leagueBoard = new ArrayList<>();
 
     public boolean addTeam(T team){
         if (team != null){
             leagueBoard.add(team);
             System.out.println(team.getName() + " added to the " + this.name +  " board!");
             return true;
-        } else{
-            System.out.println("Invalid data");
-            return false;
         }
+        System.out.println("Invalid data");
+        return false;
     }
+
     public void showLeagueBoard() {
         Collections.sort(leagueBoard);
         for (T t : leagueBoard){
